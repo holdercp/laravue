0d89ec4d1e311ac11b830b23152ac7c8e94788ca
holdercp <chrispholder@gmail.com>
2019-02-17 19:06:11 -0500

    Set up basic routing

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,12 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/task', 'TaskController');
+Route::get('/category/{category}/tasks', 'CategoryController');
+Route::resource('/category', 'CategoryController');
